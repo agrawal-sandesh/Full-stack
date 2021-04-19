58416473811a9b784b0052dd1551e86d1cb1f81b
Sandesh Agrawal <54537634+agrawal-sandesh@users.noreply.github.com>
2021-04-19 14:52:49 +0530

    Create App.jsx

---
+++ React.js/Components in React/Assignment 2/App.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Book from './components/Book.jsx';
+
+class App extends Component {
+  constructor() {
+    super();
+    
+    const book1 = {
+      "author": "Frank Herbert",
+      "title": "Dune",
+      "summary": "The story of a boy and a great family’s ambitious attempt to realize mankind’s most ancient and unattainable dream."
+      }
+   
+    const book2 = {
+      "author": "Mur Lafferty",
+      "title": "Six Wakes",
+      "summary": "A crew of clones awakens aboard a space ship to find they're being hunted-and any one of them could be the killer."
+      }
+   
+  const book3 = {
+      "author": "Yann Martel",
+      "title": "Life of Pi",
+      "summary": "An inspirational tale of survival, endurance, resilience and faith"
+      }
+      this.bookArr = [book1, book2, book3]
+  }
+  render() {
+    return (
+    <React.Fragment>
+      <h4 className="text-success">Bestsellers 2018</h4>
+      <div className= "row" style={{ 'marginTop': 30 }}>
+       {this.bookArr.map(book => <Book key={book.title} book={book}/>)}
+      </div>
+    </React.Fragment>
+    );
+  }
+}
+
+export default App;
