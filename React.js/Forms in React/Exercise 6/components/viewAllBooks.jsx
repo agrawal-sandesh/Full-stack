cdc0005d3f74958b280fc95b03647f832f3aab22
Sandesh Agrawal <54537634+agrawal-sandesh@users.noreply.github.com>
2021-04-19 15:24:43 +0530

    Create viewAllBooks.jsx

---
+++ React.js/Forms in React/Exercise 6/components/viewAllBooks.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+class ViewAllBooks extends React.Component{
+    render(){
+        return(
+            <h4 className="text-success">Keep Checking! New Books Added Every Week!</h4>
+        )
+    }
+}
+export default ViewAllBooks
