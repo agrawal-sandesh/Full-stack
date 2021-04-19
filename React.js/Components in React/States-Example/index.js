b2ddc1b5c6a35128724cd48e7947467d5e13cc22
Sandesh Agrawal <54537634+agrawal-sandesh@users.noreply.github.com>
2021-04-19 15:21:35 +0530

    Create index.js

---
+++ React.js/Components in React/States-Example/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Employee from './Employee.jsx';
+
+ReactDOM.render(<Employee></Employee>, document.getElementById('root'));
