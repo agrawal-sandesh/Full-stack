9095ba82aa35298acdd2efdcc4c55b50059b0abe
Sandesh Agrawal <54537634+agrawal-sandesh@users.noreply.github.com>
2021-04-19 15:28:15 +0530

    Create addbook.jsx

---
+++ React.js/Routing in React/Exercise 5/components/addbook.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+class AddBook extends React.Component{
+    render(){
+        return(
+            <h4 className="text-danger">Sorry We are still working on this feature</h4>
+        )
+    }
+}
+export default AddBook
