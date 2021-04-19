4359b09ba0d71e80ce80c4c444853b6fc03e5c01
Sandesh Agrawal <54537634+agrawal-sandesh@users.noreply.github.com>
2021-04-19 15:17:18 +0530

    Create index.js

---
+++ React.js/Components in React/Life-Cycle-Methods/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "bootstrap/dist/css/bootstrap.css";
+import"./index.css";
+import Educator from './Educator.jsx';
+
+ReactDOM.render(<Educator/>, document.getElementById('root'));
