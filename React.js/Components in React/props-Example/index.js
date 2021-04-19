2a2a43ccac10ac1f020bcf478b9307bbf7ec916f
Sandesh Agrawal <54537634+agrawal-sandesh@users.noreply.github.com>
2021-04-19 15:20:20 +0530

    Create index.js

---
+++ React.js/Components in React/props-Example/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Person from './Person.jsx';
+
+ReactDOM.render(<Person name="Jack" fruit="Apple"/>, document.getElementById('root'));
