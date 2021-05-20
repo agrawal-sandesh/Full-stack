55f45645e3e505b1d5902286e9058b30f3f05911
Sandesh Agrawal <54537634+agrawal-sandesh@users.noreply.github.com>
2021-05-20 12:16:47 +0530

    Create index.js

---
+++ React.js/Redux in React/Advance Redux(Combining Reducer)/index.js
@@ -0,0 +1,30 @@
+/**
+* index.js - root file of the application
+*/ 
+/* REACT related imports*/
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+/* REDUX related imports*/
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers'
+
+/* Styling and App's React components related imports*/
+import './index.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import App from './components/App.jsx'
+
+/* Creating the REDUX STORE const store */
+const store = createStore(rootReducer)
+
+/* Loading the <App/> root view-component of the app
+* <Provided/> by the {store}
+* inside ReactDOM.render() */
+
+ReactDOM.render(
+  <Provider store={store}>
+	  <App/>
+  </Provider>,
+document.getElementById('root')
+)
